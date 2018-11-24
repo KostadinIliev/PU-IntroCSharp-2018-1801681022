5a78e50727e78ba8b72c0af89b9f91376dc5db04
Kostadin Iliev <kokito_1912@abv.bg>
2018-11-24 15:48:58 +0200

    Included homework to week8.

---
+++ PUIntroHomeworks/Calculator/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input;
+            char[] operations = { '+', '-', '*', '/' };
+            double result = 0;
+            while (true)
+            {
+                Console.WriteLine("Do you want to calculate some numbers? (yes,no)");
+                input = Console.ReadLine();
+                if (input == "no")
+                {
+                    break;
+                }
+                else if (input == "yes")
+                {
+                    Console.WriteLine("Enter first number:");
+                    double firstNumber = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter second number:");
+                    double secondNumber = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter operation:");
+                    string inputOperation = Console.ReadLine();
+                    char operation;
+
+                    bool successfullyParsed = char.TryParse(inputOperation, out operation);
+                    while (!successfullyParsed || !operations.Contains(operation))
+                    {
+                        Console.WriteLine("Invalid input! You have to enter a character [+, -, *, /]");
+                        inputOperation = Console.ReadLine();
+                        successfullyParsed = char.TryParse(inputOperation, out operation);
+                    }
+                    switch (operation)
+                    {
+                        case '+':
+                            result = firstNumber + secondNumber;
+                            result *= 1.1;
+                            Console.WriteLine($"Result is {result:0.00}");
+                            Console.WriteLine("\n****************************\n****************************\n****************************\n");
+                            break;
+                        case '-':
+                            double maxNum = Math.Max(firstNumber, secondNumber);
+                            double minNum = Math.Min(firstNumber, secondNumber);
+                            result = Math.Pow(maxNum, 2) - minNum;
+                            Console.WriteLine($"Result is {result:0.00}");
+                            Console.WriteLine("\n****************************\n****************************\n****************************\n");
+                            break;
+                        case '*':
+                            result = firstNumber * Math.Sqrt(secondNumber);
+
+                            Console.WriteLine($"Result is {result:0.00}");
+                            Console.WriteLine("\n****************************\n****************************\n****************************\n");
+                            break;
+                        case '/':
+                            if (secondNumber == 0)
+                            {
+                                Console.WriteLine("Error! You can't devide with 0!!");
+                            }
+                            else
+                            {
+                                result = firstNumber / secondNumber;
+                                Console.WriteLine($"Result is {result:0.00}");
+                                Console.WriteLine("\n****************************\n****************************\n****************************\n");
+                            }
+                            break;
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PUIntroHomeworks/PiggyBank/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PiggyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            double sum = 0;
+
+            Console.WriteLine("Enter a real number:");
+            string input = Console.ReadLine();
+
+            int inputNum;
+            while (!int.TryParse(input, out inputNum))
+            {
+                Console.WriteLine("You have to enter a valid real number!");
+                input = Console.ReadLine();
+            }
+
+            for (int i = 1; i <= 31; i++)
+            {
+                int number = rnd.Next(1, inputNum);
+                double rootedNum = Math.Sqrt(number);
+                if (rootedNum >= 1 && rootedNum <= 300)
+                {
+                    rootedNum *= 5.1;
+                }
+                else if (rootedNum >= 301 && rootedNum <= 600)
+                {
+                    rootedNum *= 10.098;
+                }
+                else if (rootedNum >= 601 && rootedNum <= 999)
+                {
+                    rootedNum *= 100.00001;
+                }
+
+                sum += rootedNum;
+
+            }
+
+            if (sum > 0)
+            {
+                Console.WriteLine($"You managed to save ${sum:0.0000} this month!");
+            }
+            else
+            {
+
+                Console.WriteLine("You didn't manage to save any money.");
+            }
+        }
+    }
+}
